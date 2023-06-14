17b868d5be6f527f5707eb24c3c42a0a52ce2ac1
Christian Andersson Garcia Reyes <34752076+christiangarciareyes@users.noreply.github.com>
2023-06-14 11:24:22 -0500

    Delete src directory

--- src/main/java/com/mitocode/config/HateoasConfig.java
+++
@@ -1,9 +0,0 @@
-package com.mitocode.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.hateoas.config.EnableHypermediaSupport;
-
-@EnableHypermediaSupport(type = EnableHypermediaSupport.HypermediaType.HAL)
-@Configuration
-public class HateoasConfig {
-}
--- src/main/java/com/mitocode/repo/IInvoiceRepo.java
+++
@@ -1,6 +0,0 @@
-package com.mitocode.repo;
-
-import com.mitocode.model.Invoice;
-
-public interface IInvoiceRepo extends IGenericRepo<Invoice, String> {
-}
